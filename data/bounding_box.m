77d19a72b7c4b100087d5063c81b4fd9c8cbedb5
yongeun <woang1892@gmail.com>
2016-06-05 05:31:59 +0900

    tools for making bounding box

--- data/bounding_box.m
+++ data/bounding_box.m
@@ -1,31 +1,100 @@
-function [] = bounding_box()
-% TODO: Modify to use multiple images
-I = imread('bmw10/bmw10_ims/3/150197528.jpg');
-fig = figure();
-imshow(I);
-uicontrol('Style', 'pushbutton', 'String', 'Save',...
-    'Position', [20 20 100 40], 'Callback', @save);
-uicontrol('Style', 'pushbutton', 'String', 'Cancel',...
-    'Position', [140 20 100 40], 'Callback', @cancel);
-rect = [];
-while 1
-    rect = getrect(fig);
-    r = rectangle('Position', rect, 'EdgeColor', 'r');
-    uiwait(fig);
-    delete(r);
-    if rect
-        break;
+function [bounding_box] = bounding_box(image_path, parts)
+    % TODO: Modify to use multiple images
+    I = imread(image_path);
+    fig = figure();
+    set(fig, 'OuterPosition', [200 100 1300 900]);
+    imshow(I);
+    bg = uibuttongroup('Visible', 'on', ...
+        'Position', [0 0 1 .07], ...
+        'SelectionChangedFcn',@(bg,event) bselection(bg,event));
+    text_size = [60, 90, 90, 60, 70, 100, 100, 100];
+    left_padding = 30;
+    
+    bounding_box = uint16(zeros(size(parts, 2), 4));
+    
+    for part=1:size(parts, 2)
+        radiobutton(part) = uicontrol(bg, 'Style', 'radiobutton', ...
+        'String', parts(part),...
+        'FontSize', 15,...
+        'Position', [left_padding, 15, left_padding + text_size(part), 35],...
+        'HandleVisibility', 'off');
+        left_padding = left_padding + text_size(part) + 30;
     end
-end
+    
+    uicontrol('Style', 'pushbutton', 'String', 'Skip',...
+    'Position', [1050 15 100 35], 'Callback', @skip_part);
+    uicontrol('Style', 'pushbutton', 'String', 'Finish',...
+    'Position', [1180 15 100 35], 'Callback', @save);
+    
+    selected = 1;
+    set(bg, 'SelectedObject', radiobutton(selected));
+    r = rectangle('Position', bounding_box(selected,:), 'EdgeColor', 'r');
+    while 1
+        try
+            rect = getrect(fig);
+        catch
+            break;
+        end
+        finished = 0;
+        if rect
+            finished = select_next_radio(rect);
+        end
+        if finished == 1
+            break;
+        end
+    end
+    
+    try
+        delete(r);
+        close(fig);
+    catch
+    end
+    
+    function bselection(~,event)
+        selected = find(ismember(parts, event.NewValue.String));
+        try
+            delete(r);
+        catch
+        end
+        r = rectangle('Position', bounding_box(selected,:), 'EdgeColor', 'r');
+    end
 
-% TODO: save coordinate in a proper format
-disp(rect);
+    
 
-    function [] = save(~, ~)
-        uiresume();
+    
+    function finished = select_next_radio(rect)
+        bounding_box(selected, :) = rect;
+        if selected ~= size(parts,2)
+            skip_part(1, 1);
+            finished = 0;
+        else
+            finished = 1;
+        end
+
     end
-    function [] = cancel(~, ~)
-        rect = [];
-        uiresume();
+
+    function save(~, ~)
+        try
+            delete(r);
+            close(fig);
+        catch
+        end
     end
+
+    function skip_part(~,~)
+        if selected ~= size(parts,2)
+            selected = selected + 1;
+            set(bg, 'SelectedObject', radiobutton(selected));
+            try
+                delete(r);
+            catch
+            end
+            r = rectangle('Position', bounding_box(selected,:), 'EdgeColor', 'r');
+        else
+            close(fig);
+        end
+    end
+
+    
 end
+    
---
+++ data/running.m
@@ -0,0 +1,21 @@
+parts = {'grill', 'headlight', 'backlight', 'door', 'fender', 'front view', 'back view', 'side view'};
+
+root_path = 'raw';
+
+car_list = dir(root_path);
+
+for idx=3:size(car_list, 1)
+    dir_path = fullfile(root_path, car_list(idx).name);
+    images = dir(dir_path);
+    
+    for imgIdx=3:size(images, 1)
+        bbox = bounding_box(fullfile(dir_path, images(imgIdx).name), parts);
+        str = strsplit(dir_path, {'/', '\'});
+        
+        file_name = strcat(str(end), '-', num2str(imgIdx-2));
+        file_name = file_name{1};
+        
+        write_xml(file_name, parts, bbox);
+    end        
+end
+   
